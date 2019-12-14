70fd667c2ef3e7382e9bd0ce56086f85325dbb01
Vivek2.Dubey <vivek2.dubey@ril.com>
2019-12-14 23:58:31 +0530

    Student Deleted

--- collegems/student/src/main/java/com/college/student/dao/StudentDAO.java
+++
@@ -1,9 +0,0 @@
-package com.college.student.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.college.student.entity.Student;
-
-public interface StudentDAO extends CrudRepository<Student, Integer>{
-
-}
--- collegems/student/src/test/java/com/college/student/StudentApplicationTest.java
+++
@@ -1,9 +0,0 @@
-package com.college.student;
-
-import org.springframework.boot.SpringApplication;
-
-
-public class StudentApplicationTest {
-
-	
-}
