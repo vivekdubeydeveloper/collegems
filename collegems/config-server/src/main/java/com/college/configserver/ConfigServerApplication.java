4e47f26404a80d0a26935644f4370045d995fc6e
Vivek2.Dubey <vivek2.dubey@ril.com>
2019-12-15 00:38:39 +0530

    Config server project

--- collegems/config-server/src/main/java/com/college/configserver/ConfigServerApplication.java
+++ collegems/config-server/src/main/java/com/college/configserver/ConfigServerApplication.java
@@ -1,5 +1,6 @@
 package com.college.configserver;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,7 +12,7 @@ public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
