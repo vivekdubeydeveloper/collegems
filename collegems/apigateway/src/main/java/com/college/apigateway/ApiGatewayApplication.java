028b02342428831da5af296bb64b064ea5a2442c
Vivek2.Dubey <vivek2.dubey@ril.com>
2019-12-16 23:21:22 +0530

    change in test cases

--- collegems/apigateway/src/main/java/com/college/apigateway/ApiGatewayApplication.java
+++ collegems/apigateway/src/main/java/com/college/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,18 @@ package com.college.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages="com.college.apigateway.*")
 @SpringBootApplication
 @EnableZuulProxy
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@EnableHystrix
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
