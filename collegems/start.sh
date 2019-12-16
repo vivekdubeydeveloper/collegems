b1068a900858ae9742e97719e73b84889fa793c8
unknown <vivek.dubeydeveloper@gmail.com>
2019-12-17 00:54:30 +0530

    script

--- collegems/start.sh
+++ collegems/start.sh
@@ -1,10 +1,10 @@
 export PROJECT_PATH=.
 sudo chmod -R 755 .
 #run the projects
-java -jar $PROJECT_PATH/config-server/target/config-server-1.0.0.jar 
-java -jar $PROJECT_PATH/servicediscovery/target/servicediscovery-1.0.0.jar
+nohup java -jar $PROJECT_PATH/config-server/target/config-server-1.0.0.jar &
+nohup java -jar $PROJECT_PATH/servicediscovery/target/servicediscovery-1.0.0.jar &
 
-#disown
+disown
 
 
 
