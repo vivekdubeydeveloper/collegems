e17af5a6c150ab83ab2b797313a9d3befed0239c
unknown <vivek.dubeydeveloper@gmail.com>
2019-12-17 01:20:25 +0530

    Script

--- collegems/start.sh
+++ collegems/start.sh
@@ -3,9 +3,18 @@ sudo chmod -R 755 .
 #run the projects
 nohup java -jar $PROJECT_PATH/config-server/target/config-server-1.0.0.jar &
 echo "waiting for 20 second"
-sleep 20;
+sleep 20
 nohup java -jar $PROJECT_PATH/servicediscovery/target/servicediscovery-1.0.0.jar &
 
+echo "waiting for 20 second"
+sleep 20
+nohup java -jar $PROJECT_PATH/apigateway/target/apigateway-1.0.0.jar &
+nohup java -jar $PROJECT_PATH/subject/target/subject-1.0.0.jar &
+nohup java -jar $PROJECT_PATH/teacher/target/teacher-1.0.0.jar &
+nohup java -jar $PROJECT_PATH/student-producer/target/student-producer-1.0.0.jar &
+nohup java -jar $PROJECT_PATH/student-consumer/target/student-consumer-1.0.0.jar &
+echo "waiting for 30 second"
+sleep 30
 disown
 
 
