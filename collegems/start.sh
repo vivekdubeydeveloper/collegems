5a965a0a7ed9be8ace2cca0d90a658c6d277c618
unknown <vivek.dubeydeveloper@gmail.com>
2019-12-17 00:19:46 +0530

    script

--- collegems/start.sh
+++ collegems/start.sh
@@ -1,9 +1,9 @@
 export PROJECT_PATH=.
-mvn clean install
 #run the projects
-nohup java -jar $PROJECT_PATH/servicediscovery/target/servicediscovery-0.0.1-SNAPSHOT.jar &
-disown
 nohup java -jar $PROJECT_PATH/config-server/target/config-server-0.0.1-SNAPSHOT.jar &
+nohup java -jar $PROJECT_PATH/servicediscovery/target/servicediscovery-0.0.1-SNAPSHOT.jar &
+
 disown
+
 
 
