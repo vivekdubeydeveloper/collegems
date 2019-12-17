242c2b4feaa428c4601350f46225f1163043b1fa
unknown <vivek.dubeydeveloper@gmail.com>
2019-12-17 23:01:12 +0530

    configuration file

--- collegems/start.sh
+++ collegems/start.sh
@@ -11,15 +11,21 @@ sleep 20
 nohup java -jar $PROJECT_PATH/apigateway/target/apigateway-1.0.0.jar &
 
 
-nohup java -jar $PROJECT_PATH/subject/target/subject-1.0.0.jar --server.port=8081 &
-nohup java -jar $PROJECT_PATH/teacher/target/teacher-1.0.0.jar --server.port=8082 &
-nohup java -jar $PROJECT_PATH/student-producer/target/student-producer-1.0.0.jar --server.port=8083 &
-nohup java -jar $PROJECT_PATH/student-consumer/target/student-consumer-1.0.0.jar  --server.port=8084 &
+nohup java -jar -Dserver.port=8081 $PROJECT_PATH/subject/target/subject-1.0.0.jar &
 
-nohup java -jar $PROJECT_PATH/subject/target/subject-1.0.0.jar --server.port=8085 &
-nohup java -jar $PROJECT_PATH/teacher/target/teacher-1.0.0.jar --server.port=8086 &
-nohup java -jar $PROJECT_PATH/student-producer/target/student-producer-1.0.0.jar --server.port=8087 &
-nohup java -jar $PROJECT_PATH/student-consumer/target/student-consumer-1.0.0.jar  --server.port=8088 &
+nohup java -jar -Dserver.port=8082 $PROJECT_PATH/teacher/target/teacher-1.0.0.jar &
+
+nohup java -jar -Dserver.port=8083 $PROJECT_PATH/student-producer/target/student-producer-1.0.0.jar  &
+
+nohup java -jar -Dserver.port=8084 $PROJECT_PATH/student-consumer/target/student-consumer-1.0.0.jar  &
+
+nohup java -jar -Dserver.port=8085 $PROJECT_PATH/subject/target/subject-1.0.0.jar &
+
+nohup java -jar -Dserver.port=8086 $PROJECT_PATH/teacher/target/teacher-1.0.0.jar &
+
+nohup java -jar -Dserver.port=8087 $PROJECT_PATH/student-producer/target/student-producer-1.0.0.jar &
+
+nohup java -jar -Dserver.port=8088 $PROJECT_PATH/student-consumer/target/student-consumer-1.0.0.jar &
 
 echo "waiting for 30 second"
 sleep 30
