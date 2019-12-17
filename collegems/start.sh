563a264220270fc35c4c6ab45edf2fa980880215
unknown <vivek.dubeydeveloper@gmail.com>
2019-12-17 23:09:44 +0530

    configuration file

--- collegems/start.sh
+++ collegems/start.sh
@@ -1,9 +1,14 @@
 export PROJECT_PATH=.
+
 sudo chmod -R 755 .
+
 #run the projects
 nohup java -jar $PROJECT_PATH/config-server/target/config-server-1.0.0.jar &
+
 echo "waiting for 20 second"
+
 sleep 20
+
 nohup java -jar $PROJECT_PATH/servicediscovery/target/servicediscovery-1.0.0.jar &
 
 echo "waiting for 20 second"
