5ca7e1125a676bb95b24cfee366877a827c7ed4b
Vivek2.Dubey <vivek2.dubey@ril.com>
2019-12-18 16:34:38 +0530

    add monitoring

--- collegems/servicediscovery/src/main/java/com/college/servicediscovery/SpringEurekaServerApplication.java
+++ collegems/servicediscovery/src/main/java/com/college/servicediscovery/SpringEurekaServerApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 @ComponentScan(basePackages = "com.college.servicediscovery.*")
 @SpringBootApplication
 @EnableEurekaServer
+@EnableAdminServer
 public class SpringEurekaServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEurekaServerApplication.class, args);
