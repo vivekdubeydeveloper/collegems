a8d0340a42029b4ff5409a45c1fb2d2d12adfdd3
unknown <vivek.dubeydeveloper@gmail.com>
2019-12-19 08:57:22 +0530

    for monitoring

--- collegems/servicediscovery/src/main/java/com/college/servicediscovery/SpringEurekaServerApplication.java
+++ collegems/servicediscovery/src/main/java/com/college/servicediscovery/SpringEurekaServerApplication.java
@@ -1,7 +1,9 @@
 package com.college.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -11,6 +13,8 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 @SpringBootApplication
 @EnableEurekaServer
 @EnableAdminServer
+@EnableDiscoveryClient
+@EnableAutoConfiguration
 public class SpringEurekaServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEurekaServerApplication.class, args);
